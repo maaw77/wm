db43f5f0a200c042fddf599c3a62ad38dc3a3aaa
Alexander Marareskul <maaw77@gmail.com>
2025-12-20 19:01:45 +0300

    feat(config): add config flag

--- cmd/wm/main.go
+++ cmd/wm/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"flag"
 	"log/slog"
 	"net/http"
 	"os"
@@ -34,7 +35,11 @@ func main() {
 		Level: slog.LevelInfo,
 	})))
 
-	config.InitConfig("./config/config.yaml")
+	var pathConfig string
+
+	flag.StringVar(&pathConfig, "config", "./config/config.yaml", "the path to the configuration file")
+	flag.Parse()
+	config.InitConfig(pathConfig)
 
 	store := storage.NewStorage()
 	storagePath := filepath.Join(config.GetConfiguredStorageDataDir(), config.GetConfiguredStorageFileName())
